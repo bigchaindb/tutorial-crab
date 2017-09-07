21c638e93d74b0c824165a43321d5d578f1dd3fe
Jernej Pregelj <jernej.pregelj88@gmail.com>
2017-09-07 11:26:37 +0200

    updated proxy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 aws s3 rm s3://tutorials.bigchaindb.com/crab --recursive
-aws s3 cp ./public s3://tutorials.bigchaindb.com/crab/public --acl public-read --cache-control 'public, max-age=3600'
-aws s3 cp ./src/index.html s3://tutorials.bigchaindb.com/crab/index.html --acl public-read --cache-control 'public, max-age=3600'
+aws s3 cp ./public s3://tutorials.bigchaindb.com/crab/public --recursive --acl public-read --cache-control 'public, max-age=0'
+aws s3 cp ./src/index.html s3://tutorials.bigchaindb.com/crab/index.html --acl public-read --cache-control 'public, max-age=0'
